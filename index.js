162f754e2bbe5a80c880260fc70e6f9c7400e6d1
Máté Farkas <farkas.mate38@gmail.com>
2016-01-28 00:18:18 +0100

    build basic wrapper

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
 'use strict';
-module.exports = function (str, opts) {
-	if (typeof str !== 'string') {
-		throw new TypeError('Expected a string');
+
+const request = require('request-promise');
+
+const allowedEndpoints = [
+	'episodes',
+	'genres',
+	'movies',
+	'people',
+	'search',
+	'seasons',
+	'shows'
+];
+
+class Trakt {
+	constructor(clientId) {
+		this.api = 'http://api.staging.trakt.tv/';
+		this.options = {
+			headers: {
+				'Content-Type': 'application/json',
+				'User-Agent': 'node-trakt',
+				'trakt-api-key': clientId,
+				'trakt-api-version': '2'
+			},
+			json: true
+		};
 	}
 
-	opts = opts || {};
+	isAllowedEndpoint(endpoint) {
+		return allowedEndpoints.indexOf(endpoint.split('/')[0]) > -1;
+	}
 
-	return str + ' & ' + (opts.postfix || 'rainbows');
-};
+	getOptionsWithURI(endpoint) {
+		this.options.uri = this.api + endpoint;
+
+		return this.options;
+	}
+
+	send(endpoint) {
+		let options;
+
+		if (!this.isAllowedEndpoint(endpoint)) {
+			throw new Error(`Endpoint ${endpoint.split('/')[0]} not supported`);
+		}
+
+		options = this.getOptionsWithURI(endpoint);
+
+		return request(options);
+	}
+}
+
+for (let endpoint of allowedEndpoints) {
+	Trakt.prototype[endpoint] = function (params) {
+		return this.send(`${endpoint}/${params}`);
+	};
+}
