f0c65bcb57895b85be8b9da62ed5fc03bb25d23a
Máté Farkas <farkas.mate38@gmail.com>
2016-01-28 00:44:33 +0100

    document code

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 const request = require('request-promise');
 const _ = require('lodash');
 
+/**
+ * The list of endpoints that are currently supported by this wrapper
+ *
+ * @type {string[]}
+ */
 const allowedEndpoints = [
 	'episodes',
 	'genres',
@@ -13,7 +18,17 @@ const allowedEndpoints = [
 	'shows'
 ];
 
+/**
+ * Trakt main class
+ */
 class Trakt {
+	/**
+	 * Initializes the trakt object.
+	 * Accepts an options parameter, which specifies the client ID
+	 * and if the consumer wants to interact with the staging API.
+	 *
+	 * @param options
+	 */
 	constructor(options) {
 		let defaults = {
 			clientId: '',
@@ -34,16 +49,36 @@ class Trakt {
 		};
 	}
 
+	/**
+	 * Checks if the requested endpoint is supported by this wrapper
+	 *
+	 * @param endpoint
+	 * @returns {boolean}
+	 */
 	isAllowedEndpoint(endpoint) {
 		return allowedEndpoints.indexOf(endpoint.split('/')[0]) > -1;
 	}
 
+	/**
+	 * Returns the options object with the specified endpoint included
+	 * as an URI.
+	 *
+	 * @param endpoint
+	 * @returns {{headers: {Content-Type: string, User-Agent: string, trakt-api-key: string, trakt-api-version: string}, json: boolean}|*}
+	 */
 	getOptionsWithURI(endpoint) {
 		this.options.uri = this.api + endpoint;
 
 		return this.options;
 	}
 
+	/**
+	 * Send a request to the Trakt API.
+	 * Returns a promise.
+	 *
+	 * @param endpoint
+	 * @returns {Promise}
+	 */
 	send(endpoint) {
 		let options;
 
@@ -57,6 +92,9 @@ class Trakt {
 	}
 }
 
+/**
+ * Register all allowed endpoints functions into the Trakt class
+ */
 for (let endpoint of allowedEndpoints) {
 	Trakt.prototype[endpoint] = function (params) {
 		return this.send(`${endpoint}/${params}`);
